5863efb718330fbae7841b495a2e7b559a892723
Pamcripty <sos.asmr@gmail.com>
2026-09-13 16:46:32 +0200

    Add sw.js for Nora game

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'meraviglie-v3';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./app-icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET'||new URL(event.request.url).origin!==self.location.origin)return;if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(response=>{if(response.ok){const copy=response.clone();event.waitUntil(caches.open(CACHE).then(cache=>cache.put(event.request,copy)));}return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));return;}event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));});
