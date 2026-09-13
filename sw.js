9a43c9f9bc9547eaee7e02990eca6ead47103e26
Pamcripty <sos.asmr@gmail.com>
2026-09-13 17:36:54 +0200

    Refresh offline cache for new mini-games

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meraviglie-v3';
+const CACHE = 'meraviglie-v4';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./app-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
